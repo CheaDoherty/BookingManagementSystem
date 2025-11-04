107cb51dd7dd2e546c3f1ad8ec7828ac5b296202
Shea Doherty <liamcheadoherty@gmail.com>
2025-11-04 21:12:45 +0000

    Adding base BookingService.cs

---
+++ BookingManagementSystem/Services/BookingService.cs
@@ -0,0 +1,54 @@
+using BookingManagementSystem.Infrastructure.Data;
+using BookingManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingManagementSystem.Services;
+
+//TODO - Add validation,null handling & error handling
+//TODO - Change return types to something with 'Status' and 'Result'
+public class BookingService : IBookingService
+{
+    private readonly BookingContext _context;
+
+    public BookingService(BookingContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Booking> GetBookingAsync(Guid id)
+    {
+        return await _context.Bookings.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Booking>> GetBookingsAsync()
+    {
+        return await _context.Bookings.ToListAsync();
+    }
+
+    public async Task<IEnumerable<Booking>> GetBookingsByUserId(Guid userId)
+    {
+        return await _context.Bookings.Where(b => b.UserId == userId).ToListAsync();
+    }
+
+    public async Task<Booking> CreateBookingAsync(Booking booking)
+    {
+        _context.Bookings.Add(booking);
+        await _context.SaveChangesAsync();
+
+        return booking;
+    }
+
+    public async Task<Booking> UpdateBookingAsync(Booking booking)
+    {
+        _context.Bookings.Update(booking);
+        await _context.SaveChangesAsync();
+        return booking;
+    }
+
+    public async Task<Booking> DeleteBookingAsync(Booking booking)
+    {
+        _context.Bookings.Remove(booking);
+        await _context.SaveChangesAsync();
+        return booking;
+    }
+}
---
+++ BookingManagementSystem/Services/IBookingService.cs
@@ -0,0 +1,14 @@
+using BookingManagementSystem.Models;
+
+namespace BookingManagementSystem.Services;
+
+public interface IBookingService
+{
+    public Task<Booking> GetBookingAsync(Guid id);
+    public Task<IEnumerable<Booking>> GetBookingsAsync();
+    public Task<IEnumerable<Booking>> GetBookingsByUserId(Guid userId);
+    public Task<Booking> CreateBookingAsync(Booking booking);
+    public Task<Booking> UpdateBookingAsync(Booking booking);
+    public Task<Booking> DeleteBookingAsync(Booking booking);
+    
+}
